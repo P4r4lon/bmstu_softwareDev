97a90d334eb38914698bd2544df5b87fecd784b0
DD <68599705+P4r4lon@users.noreply.github.com>
2022-04-21 06:12:45 +0300

    lab3 done

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,21 +1,108 @@
 #include <jni.h>
 #include <string>
+
 #include <android/log.h>
-#define LOG_INFO(...) __android_log_print(ANDROID_LOG_INFO, "fclient_ndk", __VA_ARGS__)
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/android_sink.h>
 
-
-#include "spdlog/spdlog.h"
-#include "spdlog/sinks/android_sink.h"
-auto logger = spdlog::android_logger_mt("android", "fclient_ndk");
-
-
-#include <mbedtls/entropy.h>
-#include <mbedtls/ctr_drbg.h>
+#include "mbedtls/entropy.h"
+#include "mbedtls/ctr_drbg.h"
 #include <mbedtls/des.h>
+
 mbedtls_entropy_context entropy;
 mbedtls_ctr_drbg_context ctr_drbg;
-char *personalization = "fclient-sample-app";
+char *personalization = "lab-sample-app";
 
+#define LOG_INFO(...) __android_log_print(ANDROID_LOG_INFO, "lab_ndk", __VA_ARGS__)
+#define SLOG_INFO(...) logger->info(__VA_ARGS__)
+
+auto logger = spdlog::android_logger_mt("android", "lab_ndk");
+
+JavaVM *gJvm = nullptr;
+
+JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *pJvm, void *reserved) {
+    gJvm = pJvm;
+    return JNI_VERSION_1_6;
+}
+
+JNIEnv *getEnv(bool &detach) {
+    JNIEnv *env = nullptr;
+    int status = gJvm->GetEnv((void **)&env, JNI_VERSION_1_6);
+    detach = false;
+    if (status == JNI_EDETACHED) {
+        status = gJvm->AttachCurrentThread(&env, nullptr);
+        if (status < 0) {
+            return nullptr;
+        }
+
+        detach = true;
+    }
+
+    return env;
+}
+
+void releaseEnv(bool detach, JNIEnv* env) {
+    if (detach && (gJvm != nullptr)) {
+        gJvm->DetachCurrentThread();
+    }
+}
+
+extern "C" JNIEXPORT jboolean JNICALL
+Java_com_example_myapplication_MainActivity_transaction(JNIEnv *xenv, jobject xthiz, jbyteArray xtrd) {
+    jobject thiz = xenv->NewGlobalRef(xthiz);
+    jbyteArray trd = (jbyteArray)xenv->NewGlobalRef(xtrd);
+
+    std::thread t([thiz, trd] {
+        bool detach = false;
+        JNIEnv *env = getEnv(detach);
+        jclass cls = env->GetObjectClass(thiz);
+        jmethodID id = env->GetMethodID(cls, "enterPin", "(ILjava/lang/String;)Ljava/lang/String;");
+
+        uint8_t *p = (uint8_t*)env->GetByteArrayElements(trd, 0);
+        jsize sz = env->GetArrayLength(trd);
+
+        if ((sz != 9) || (p[0] != 0x9F) || (p[1] != 0x02) || (p[2] != 0x06)) {
+            return false;
+        }
+
+        char buffer[13];
+        for (int i = 0; i < 6; ++i) {
+            uint8_t n = *(p + 3 + i);
+            buffer[i * 2] = ((n & 0xF0) >> 4) + '0';
+            buffer[i * 2 + 1] = (n & 0x0F) + '0';
+        }
+
+        buffer[12] = 0x00;
+        jstring amount = (jstring) env->NewStringUTF(buffer);
+        int ptc = 3;
+        while (ptc > 0) {
+            jstring pin = (jstring) env->CallObjectMethod(thiz, id, ptc, amount);
+            const char *utf = env->GetStringUTFChars(pin, nullptr);
+
+            env->ReleaseStringUTFChars(pin, utf);
+            if ((utf != nullptr) && (strcmp(utf, "1234") == 0)) {
+                break;
+            }
+
+            ptc--;
+        }
+
+
+        id = env->GetMethodID(cls, "transactionResult", "(Z)V");
+        env->CallVoidMethod(thiz, id, ptc > 0);
+
+        env->ReleaseByteArrayElements(trd, (jbyte *)p, 0);
+
+        env->DeleteGlobalRef(thiz);
+        env->DeleteGlobalRef(trd);
+
+        releaseEnv(detach, env);
+        return true;
+    });
+
+    t.detach();
+    return true;
+}
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_myapplication_MainActivity_stringFromJNI(
@@ -27,14 +114,6 @@ Java_com_example_myapplication_MainActivity_stringFromJNI(
     return env->NewStringUTF(hello.c_str());
 }
 
-extern "C" JNIEXPORT jint JNICALL
-Java_com_example_myapplication_MainActivity_initRing(JNIEnv *env, jclass) {
-    mbedtls_entropy_init( &entropy );
-    mbedtls_ctr_drbg_init( &ctr_drbg );
-    return mbedtls_ctr_drbg_seed( &ctr_drbg , mbedtls_entropy_func, &entropy,
-                                  (const unsigned char *) personalization,
-                                  strlen( personalization ) );
-}
 
 extern "C" JNIEXPORT jbyteArray JNICALL
 Java_com_example_myapplication_MainActivity_randomBytes(JNIEnv *env, jclass, jint no) {
@@ -116,3 +195,13 @@ Java_com_example_myapplication_MainActivity_decrypt(JNIEnv *env,
     env->ReleaseByteArrayElements(data, pdata, 0);
     return dout;
 }
+extern "C"
+JNIEXPORT jint JNICALL
+Java_com_example_myapplication_MainActivity_initRng(JNIEnv *env, jclass) {
+        mbedtls_entropy_init( &entropy );
+        mbedtls_ctr_drbg_init( &ctr_drbg );
+        return mbedtls_ctr_drbg_seed( &ctr_drbg , mbedtls_entropy_func, &entropy,
+                                      (const unsigned char *) personalization,
+                                      strlen( personalization ) );
+    }
+
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,92 +1,110 @@
 package com.example.myapplication;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.myapplication.databinding.ActivityMainBinding;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import android.app.Activity;
+import android.content.Intent;
 
 import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
 
-public class MainActivity extends AppCompatActivity {
-    ActivityResultLauncher activityResultLauncher;
+public class MainActivity extends AppCompatActivity implements TransactionEvents {
 
-    // Used to load the 'myapplication' library on application startup.
+    ActivityResultLauncher<Intent> activityResultLauncher;
+
     static {
         System.loadLibrary("myapplication");
         System.loadLibrary("mbedcrypto");
     }
 
-    private ActivityMainBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        activityResultLauncher  = registerForActivityResult(
+        activityResultLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
-                new ActivityResultCallback<ActivityResult>() {
-                    @Override
-                    public void onActivityResult(ActivityResult result) {
-                        if (result.getResultCode() == Activity.RESULT_OK) {
-                            Intent data = result.getData();
-                            // обработка результата
-                            String pin = data.getStringExtra("pin");
-                            Toast.makeText(MainActivity.this, pin,
-                                    Toast.LENGTH_SHORT).show();
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        pin = data.getStringExtra("pin");
+                        synchronized (MainActivity.this) {
+                            MainActivity.this.notifyAll();
                         }
                     }
                 });
 
-        int res = initRing();
-        byte[] v = randomBytes(10);
+        int res = initRng();
+        byte[] rnd = randomBytes(10);
+    }
 
+    public void onButtonClick(View view) {
+//        new Thread(() -> {
+//            try {
+//                byte[] trd = stringToHex("9F0206000000000100");
+//                transaction(trd);
+//            } catch (Exception exception) {
+//                Log.println(Log.ERROR, "MtLog", Arrays.toString(exception.getStackTrace()));
+//            }
+//        }).start();
+        byte[] trd = stringToHex("9F0206000000000100");
+        transaction(trd);
+    }
 
-//        Intent it = new Intent(this, PinpadActivity.class);
-//        activityResultLauncher.launch(it);
+    private String pin;
+
+    @Override
+    public String enterPin(int ptc, String amount) {
+        pin = new String();
+
+        Intent intent = new Intent(MainActivity.this, PinpadActivity.class);
+        intent.putExtra("ptc", ptc);
+        intent.putExtra("amount", amount);
+
+        synchronized (MainActivity.this) {
+            activityResultLauncher.launch(intent);
+            try {
+                MainActivity.this.wait();
+            } catch (Exception exception) {
+                Log.println(Log.ERROR, "MtLog", exception.getMessage());
+            }
+        }
+
+        return pin;
+    }
+    @Override
+    public void transactionResult(boolean result) {
+        runOnUiThread(() -> {
+            Toast.makeText(MainActivity.this, result ? "ok" : "failed", Toast.LENGTH_SHORT).show();
+        });
     }
 
 
-    public static byte[] stringToHex(String s)
-    {
+    public static byte[] stringToHex(String s) {
         byte[] hex;
-        try
-        {
+        try {
             hex = Hex.decodeHex(s.toCharArray());
-        }
-        catch (DecoderException ex)
-        {
+        } catch (DecoderException ex) {
             hex = null;
         }
         return hex;
     }
 
-    public void onButtonClick (View v)
-    {
-        Intent it = new Intent(this, PinpadActivity.class);
-        activityResultLauncher.launch(it);
-    }
-    /**
-     * A native method that is implemented by the 'myapplication' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
-    public native int initRing();
+    public static native int initRng();
+
     public static native byte[] randomBytes(int no);
+
     public static native byte[] encrypt(byte[] key, byte[] data);
+
     public static native byte[] decrypt(byte[] key, byte[] data);
+
+    public native boolean transaction(byte[] trd);
+
 }
